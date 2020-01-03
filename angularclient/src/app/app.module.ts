ffa590ac2b4c9a71fbab0d04cc36d20b2d28e47c
Zach Martin <zach@zach-martin.com>
2020-01-02 21:49:08 -0500

    Adding angular client.

--- angularclient/src/app/app.module.ts
+++ angularclient/src/app/app.module.ts
@@ -3,15 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import {RouterModule} from '@angular/router';
 
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        RouterModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
