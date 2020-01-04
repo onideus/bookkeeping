46e41b6ca42480219acac1e81c93c9adabebb8ec
Zach Martin <zach@zach-martin.com>
2020-01-03 22:30:19 -0500

    Update to force application to work on home network.

--- angularclient/src/app/service/user-service.service.ts
+++ angularclient/src/app/service/user-service.service.ts
@@ -9,7 +9,7 @@ export class UserService {
   private usersUrl: string;
 
   constructor(private http: HttpClient) {
-    this.usersUrl = 'http://localhost:8080/users';
+    this.usersUrl = 'http://192.168.1.100:8080/users';
   }
 
   public findAll(): Observable<User[]> {
