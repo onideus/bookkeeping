1d12039ec76d6a1f1f540832a4bb4d65047bf7b9
Zach Martin <zach@zach-martin.com>
2020-01-05 00:18:20 -0500

    Wiring additional hibernate entities.

--- src/main/java/com/oni/bookkeeping/controller/UserController.java
+++ src/main/java/com/oni/bookkeeping/controller/UserController.java
@@ -3,9 +3,11 @@ package com.oni.bookkeeping.controller;
 import com.oni.bookkeeping.entity.Media;
 import com.oni.bookkeeping.entity.MediaType;
 import com.oni.bookkeeping.entity.User;
+import com.oni.bookkeeping.entity.UserTrackedMedia;
 import com.oni.bookkeeping.repository.MediaRepository;
 import com.oni.bookkeeping.repository.MediaTypeRepository;
 import com.oni.bookkeeping.repository.UserRepository;
+import com.oni.bookkeeping.repository.UserTrackedMediaRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,6 +21,7 @@ public class UserController {
     private final UserRepository userRepository;
     private final MediaTypeRepository mediaTypeRepository;
     private final MediaRepository mediaRepository;
+    private final UserTrackedMediaRepository userTrackedMediaRepository;
 
     @GetMapping("/users")
     public List<User> getUsers() {
@@ -35,6 +38,12 @@ public class UserController {
     public List<Media> getMedia() {
         List<Media> media = (List<Media>) mediaRepository.findAll();
         return media;
+    }
+
+    @GetMapping("/tracked")
+    public List<UserTrackedMedia> getUserMedia() {
+        List<UserTrackedMedia> userTrackedMedia = (List<UserTrackedMedia>) userTrackedMediaRepository.findAll();
+        return userTrackedMedia;
     }
 
     @PostMapping("/users")
---
+++ src/main/java/com/oni/bookkeeping/entity/UserTrackedMedia.java
@@ -0,0 +1,29 @@
+package com.oni.bookkeeping.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity(name = "UserTrackedMedia")
+@Table(name = "user_tracked_media")
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Data
+public class UserTrackedMedia {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+    private User user;
+    @OneToOne
+    @JoinColumn(name = "media_id", referencedColumnName = "id")
+    private Media media;
+    private boolean wishlist;
+    private boolean owned;
+}
